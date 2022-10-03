85b7e1465df999479eb06b4ebfbf1ff6b71aaa36
Aleksandrov Ivan <aleksandrov262003@gmail.com>
2022-10-03 13:51:31 +0400

    upd

--- js/scripts.js
+++ js/scripts.js
@@ -14,6 +14,32 @@ jQuery.noConflict()(function($) {
     window.fl_theme = fl_theme;
     fl_theme.window = $(window);
 
+    // 
+
+    var tabs = $('.tabs');
+    var selector = $('.tabs').find('a').length;
+    //var selector = $(".tabs").find(".selector");
+    var activeItem = tabs.find('.active');
+    var activeWidth = activeItem.innerWidth();
+    $(".selector").css({
+      "left": activeItem.position.left + "px", 
+      "width": activeWidth + "px"
+    });
+    
+    $(".tabs").on("click","a",function(e){
+      e.preventDefault();
+      $('.tabs a').removeClass("active");
+      $(this).addClass('active');
+      var activeWidth = $(this).innerWidth();
+      var itemPos = $(this).position();
+      $(".selector").css({
+        "left":itemPos.left + "px", 
+        "width": activeWidth + "px"
+      });
+    });
+
+    // 
+
 
 //Comment
     function fl_comment_insert(){
